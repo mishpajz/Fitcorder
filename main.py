324be721773cdf3a17807e635a7249f51564c27d
Michal Dobes <developer@michaldobes.eu>
2023-02-27 19:35:26 +0100

    Introduce storing data into file using pickle

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
 from flask_table import Table, Col
 from markupsafe import Markup, escape
+import pickle
+import os
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 class CheckboxCol(Col):
     def td_format(self, content):
@@ -27,33 +30,77 @@ class TimeTable(Table):
     thursday = CheckboxCol('thu', td_html_attrs={'class': 'checkbox'})
     friday = CheckboxCol('fri', td_html_attrs={'class': 'checkbox'})
 
+def store_data(data, filename):
+    # create a relative path to the config directory
+    filepath = os.path.join("config", filename)
+    try:
+        # check if the config directory exists
+        if not os.path.exists("config"):
+            # create a new directory
+            os.mkdir("config")
+        # open the file for writing
+        file = open(filepath, "wb")
+        # write the dictionary to the file
+        pickle.dump(data, file)
+    except Exception as e:
+        # handle any other exception
+        print(f"An error occurred: {e}")
+    finally:
+        # close the file if it is opened
+        if "file" in locals():
+            file.close()
+
+def load_data(filename):
+    # create a relative path to the config directory
+    filepath = os.path.join("config", filename)
+    try:
+        # open the file for reading
+        file = open(filepath, "rb")
+        # load the dictionary from the file
+        data = pickle.load(file)
+    except Exception as e:
+        # handle any other exception
+        print(f"An error occurred: {e}")
+    finally:
+        # close the file if it is opened
+        if "file" in locals():
+            file.close()
+            return data
+
+
 @app.route('/T9-155/', methods=['GET', 'POST'])
 @app.route('/T9-105/', methods=['GET', 'POST'])
 @app.route('/T9-107/', methods=['GET', 'POST'])
 def timetable():
+    route_name = request.url_rule.rule[1:-1]
 
     times = ['8:00', '9:40', '11:20', '13:00', '14:40', '16:20', '18:00', '19:40']
     
     if request.method == "GET":
         # Set initial values for checkboxes
+
+        data = load_data(route_name)
+        if not data: # check if data is empty
+            data = {} # create an empty dictionary
+
         items = []
 
         for i in range(8):  
             items.append(dict(time=times[i], 
-                      monday=(i, False),
-                      tuesday=(i, False),
-                      wednesday=(i, True),
-                      thursday=(i, False),
-                      friday=(i, False)))
+                      monday=(i, str(i) in data.get('mon', [])),
+                      tuesday=(i, str(i) in data.get('tue', [])),
+                      wednesday=(i, str(i) in data.get('wed', [])),
+                      thursday=(i, str(i) in data.get('thu', [])),
+                      friday=(i, str(i) in data.get('fri', []))
+                      ))
         table = TimeTable(items)
         return render_template("timetable.html", table=table)
     
     elif request.method == "POST":
         # Get values of checkboxes
         data = request.form.to_dict(False)
-        print(request.url_rule)
-        print(data) # You can do something with this data here
-        return "You submitted the form."
+        store_data(data, route_name);
+        return "Saved!"
 
 @app.route('/')
 def index():
